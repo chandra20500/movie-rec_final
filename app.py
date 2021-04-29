5bd4e4a64c5a05b32f6ddd30cdab925aceef2fa1
chandra20500 <50678178+chandra20500@users.noreply.github.com>
2021-04-30 02:39:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+import flask
+import numpy as np
+from flask import Flask, render_template
+from flask import request
+from operator import add
+import json
+import pandas as pd
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+@app.route('/')
+def my_index():
+	return flask.render_template('index.html',token = "hello world")
+
+@app.route('/result', methods = ['POST'])
+def result():
+	json_data = request.get_json()
+	movies = json_data['movie']
+	result = []
+
+	if len(movies) < 5:
+		error_msg = ["please check if you have selected atleast 5 movies"]
+		return json.dumps(error_msg)
+
+	if len(movies) > 6:
+		error_msg = ["don't select a lot of movies :("]
+		return json.dumps(error_msg)
+	
+	m1 = movies[0]
+	m2 = movies[1]
+	m3 = movies[2]
+	m4 = movies[3]
+	m5 = movies[4]
+
+	for i in range(18):
+		result.append(m1[i] + m2[i] + m3[i] + m4[i] + m5[i])
+	
+	f = open('data2.json',)
+
+	return_metric = pd.DataFrame(columns=['name', 'dot_product'])
+	return_metric.head()
+
+	data = json.load(f)
+	for i in data['movies']:
+	  return_metric.loc[len(return_metric.index)] = [i['name'], np.dot(i['genres'],result)]
+	
+	final_metric = return_metric.sort_values(by = 'dot_product', ascending = False)
+	arr = []
+
+	df = final_metric.head(10)
+	
+	for i in df.index:
+	  arr.append(df['name'][i])
+
+	jsonString = json.dumps(arr)
+	return jsonString
+
+app.run(debug=True)
---
+++ static/react/precache-manifest.8b92046caf029ee21e19fb7580d27ea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb846555df487b15c9e8dcbdc046904",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "3514d848e10bfc8a4878",
+    "url": "/static/react/css/main.adcd3008.chunk.css"
+  },
+  {
+    "revision": "0896dc123a82c1682b56",
+    "url": "/static/react/js/2.f35bd78c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/react/js/2.f35bd78c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3514d848e10bfc8a4878",
+    "url": "/static/react/js/main.072b6d0c.chunk.js"
+  },
+  {
+    "revision": "ffd75fed22706c134231",
+    "url": "/static/react/js/runtime-main.da6beee9.js"
+  }
+]);
